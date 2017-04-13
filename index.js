cf439090b463bbaa56e6ba92c835bcbee204fdc3
TheBITLINK aka BIT <me@thebitlink.com>
2017-04-13 16:24:25 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const Playlist = require('./playlist')
+const update = require('./updater')
+const path = require('path')
+const { spawn } = require('child_process')
+const { platform } = process
+
+module.exports = {
+  update,
+  /**
+   * Gets information about a video url.
+   *
+   * By default, if you don't pass any args to the executable, the following
+   * will be used: --default-search=ytsearch -i
+   *
+   * --dump-json will always be used no matter what.
+   *
+   * Unless wait is set to true, the promise will resolve when there's information
+   * on at least one video.
+   *
+   * @param {string|string[]} query - URL(s) to find.
+   * @param {string[]} args - Arguments passed to the youtube-dl executable.
+   * @param {boolean} wait - Wait for all videos before resolving the promise.
+   * @return {Promise<Playlist>} - A playlist object containing the video information.
+   */
+  getInfo (query, args, wait = false) {
+    return new Promise((resolve, reject) => {
+      let lastError
+      // Checks
+      if (!query) return reject(new Error('No query specified.'))
+      if (typeof query !== 'string' && !(query instanceof Array)) {
+        return reject(new Error('query must be a string or array'))
+      }
+      // Arguments
+      const a = ['--dump-json'].concat(args || ['--default-search=ytsearch', '-i']).concat(query)
+      // Create a playlist object
+      const pl = new Playlist()
+      // Launch the youtube-dl executable
+      const bin = path.join(__dirname, `ytdl`, `youtube-dl${platform === 'win32' ? '.exe' : ''}`)
+      const ytdl = spawn(bin, a, { maxBuffer: Infinity })
+      // Handle errors
+      ytdl.on('error', reject)
+      ytdl.stderr.on('data', d => pl.emit('error', new Error(d)))
+      // Parse incoming data
+      ytdl.stdout.on('data', d => {
+        try {
+          const data = JSON.parse(d)
+          pl.items.push(data)
+          pl.emit('video', data)
+          if (pl.items.length === 1 && !wait) {
+            // Wait 100ms for the process to exit before resolving the promise
+            // This way, if there's only a single video, the playlist will be resolved
+            // with 'partial' set to false.
+            setTimeout(() => {
+              if (pl.partial) {
+                resolve(pl)
+                pl.emit('video', data)
+              }
+            }, 100)
+          }
+        } catch (e) {
+          pl.emit('error', e)
+        }
+      })
+      // Close Event
+      ytdl.stdout.on('close', code => {
+        pl.partial = false
+        pl.emit('done')
+        // Reject the promise if there are no items and there's a error
+        if (!pl.items.length && code > 0) {
+          return reject(lastError || new Error('Process exited with code ' + code))
+        }
+        // Resolve the promise if wait is set to true or there's only one item
+        if (wait || pl.items.length <= 1) resolve(pl)
+      })
+      // Keep track of the errors
+      pl.on('error', e => { lastError = e })
+      // Add a _cancel function to kill the process
+      pl._cancel = () => ytdl.kill()
+    })
+  }
+}
---
+++ test/test.js
@@ -0,0 +1,49 @@
+const { getInfo } = require('../index')
+var chai = require('chai')
+chai.use(require('chai-events'))
+chai.use(require('chai-as-promised'))
+chai.should()
+
+describe('ytdl-getinfo', function () {
+  it('should fail when called with no query', function () {
+    return getInfo().should.be.rejected
+  })
+  
+  it('should resolve with basic info for a single video', function () {
+    this.timeout(30000)
+    return getInfo('v7BddpYYNGk').should.eventually.have.property('items').with.lengthOf(1)
+  })
+
+  it('should resolve with "partial" set to false for a single video', function () {
+    this.timeout(30000)
+    return getInfo('v7BddpYYNGk').should.eventually.have.property('partial', false)
+  })
+
+  it('should kill the process when .cancel() is called', function () {
+    this.timeout(30000)
+    return getInfo('PLwiyx1dc3P2JR9N8gQaQN_BCvlSlap7re').then(p => {
+      setTimeout(() => p.cancel(), 100)
+      return p
+    })
+    .should.eventually.emit('cancelled')
+  })
+
+  it('should resolve with "partial" set to true for playlists', function () {
+    this.timeout(30000)
+    return getInfo('PLwiyx1dc3P2JR9N8gQaQN_BCvlSlap7re').then(p => { p.cancel(); return p })
+    .should.eventually.have.property('partial', true)
+  })
+  
+  it('should wait for the entire playlist when "wait" is true', function () {
+    this.timeout(60000)
+    return getInfo('PLwiyx1dc3P2JR9N8gQaQN_BCvlSlap7re', [], true)
+    .should.eventually.have.property('items').with.lengthOf(3)
+  })
+
+  it('should emit the "done" event when all the playlist data is available', function () {
+    this.timeout(60000)
+    return getInfo('PLwiyx1dc3P2JR9N8gQaQN_BCvlSlap7re')
+    .should.eventually.emit('done')
+  })
+
+})
