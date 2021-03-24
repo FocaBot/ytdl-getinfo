8e893d135cab1f1f386453ed7f93c5e6b77786b7
Sayem Chowdhury <sayem314@gmail.com>
2021-03-24 22:46:20 +0600

    use standard.js styles

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const Playlist = require("./playlist");
-const update = require("./updater");
-const { join } = require("path");
-const { EOL } = require("os");
-const { spawn, execFile } = require("child_process");
+const Playlist = require('./playlist')
+const update = require('./updater')
+const { join } = require('path')
+const { EOL } = require('os')
+const { spawn, execFile } = require('child_process')
 
 const bin = join(
   __dirname,
-  `ytdl`,
-  `youtube-dl${process.platform === "win32" ? ".exe" : ""}`
-);
+  'ytdl',
+  `youtube-dl${process.platform === 'win32' ? '.exe' : ''}`
+)
 
 module.exports = {
   update,
@@ -28,91 +28,91 @@ module.exports = {
    * @param {boolean} wait - Wait for all videos before resolving the promise.
    * @return {Promise<Playlist>} - A playlist object containing the video information.
    */
-  getInfo(query, args, wait = false) {
+  getInfo (query, args, wait = false) {
     return new Promise((resolve, reject) => {
-      let lastError;
-      let pData = "";
+      let lastError
+      let pData = ''
       // Checks
       if (!query) {
-        return reject(new Error("No query specified."));
+        return reject(new Error('No query specified.'))
       }
-      if (typeof query !== "string" && !(query instanceof Array)) {
-        return reject(new Error("query must be a string or array"));
+      if (typeof query !== 'string' && !(query instanceof Array)) {
+        return reject(new Error('query must be a string or array'))
       }
       // Arguments
-      const a = ["--dump-json"]
-        .concat(args || ["--default-search=ytsearch", "-i", "--format=best"])
-        .concat(["-a", "-"]); // stdin
-      const q = [].concat(query);
+      const a = ['--dump-json']
+        .concat(args || ['--default-search=ytsearch', '-i', '--format=best'])
+        .concat(['-a', '-']) // stdin
+      const q = [].concat(query)
       // Create a playlist object
-      const pl = new Playlist();
+      const pl = new Playlist()
       // Launch the youtube-dl executable
-      const ytdl = spawn(bin, a, { maxBuffer: Infinity });
+      const ytdl = spawn(bin, a, { maxBuffer: Infinity })
       // Handle errors
-      ytdl.on("error", reject);
-      ytdl.stderr.on("data", (d) => pl.emit("error", new Error(d)));
+      ytdl.on('error', reject)
+      ytdl.stderr.on('data', (d) => pl.emit('error', new Error(d)))
       // Send query
-      ytdl.stdin.end(q.join(EOL), "utf8");
+      ytdl.stdin.end(q.join(EOL), 'utf8')
       // Parse incoming data
-      ytdl.stdout.on("data", (d) => {
-        pData += d;
-        if (pData.indexOf("\n") <= 0) {
-          return;
+      ytdl.stdout.on('data', (d) => {
+        pData += d
+        if (pData.indexOf('\n') <= 0) {
+          return
         }
         try {
-          const data = JSON.parse(pData);
-          pl.items.push(data);
-          pl.emit("video", data);
+          const data = JSON.parse(pData)
+          pl.items.push(data)
+          pl.emit('video', data)
           if (pl.items.length === 2 && !wait) {
             // Wait 100ms for the process to exit before resolving the promise
             // This way, if there's only 2 videos, the playlist will be resolved
             // with 'partial' set to false.
             setTimeout(() => {
               if (pl.partial) {
-                resolve(pl);
-                pl.emit("video", data);
+                resolve(pl)
+                pl.emit('video', data)
               }
-            }, 100);
+            }, 100)
           }
         } catch (e) {
-          pl.emit("error", e);
+          pl.emit('error', e)
         }
-        pData = "";
-      });
+        pData = ''
+      })
       // Close Event
-      ytdl.on("close", (code) => {
-        pl.partial = false;
-        pl.emit("done");
+      ytdl.on('close', (code) => {
+        pl.partial = false
+        pl.emit('done')
         // Reject the promise if there are no items and there's a error
         if (!pl.items.length && code > 0) {
           return reject(
-            lastError || new Error("Process exited with code " + code)
-          );
+            lastError || new Error('Process exited with code ' + code)
+          )
         }
         // Resolve the promise if wait is set to true or there's only one item
-        if (wait || pl.items.length <= 1) resolve(pl);
-      });
+        if (wait || pl.items.length <= 1) resolve(pl)
+      })
       // Keep track of the errors
-      pl.on("error", (e) => {
-        lastError = e;
-      });
+      pl.on('error', (e) => {
+        lastError = e
+      })
       // Add a _cancel function to kill the process
-      pl._cancel = () => ytdl.kill();
-    });
+      pl._cancel = () => ytdl.kill()
+    })
   },
 
   /**
    * Resolves to the version string of the youtube-dl executable
    * @return {Promise<string>}
    */
-  getVersion() {
+  getVersion () {
     return new Promise((resolve, reject) => {
-      execFile(bin, ["--version"], (error, stdout, stderr) => {
+      execFile(bin, ['--version'], (error, stdout, stderr) => {
         if (error || stderr.length) {
-          return reject(error || stderr);
+          return reject(error || stderr)
         }
-        resolve(stdout);
-      });
-    });
-  },
-};
+        resolve(stdout)
+      })
+    })
+  }
+}
--- updater.js
+++ updater.js
@@ -1,38 +1,38 @@
-const { writeFileSync } = require("fs");
-const { join } = require("path");
-const { execFileSync } = require("child_process");
-const axios = require("axios");
+const { writeFileSync } = require('fs')
+const { join } = require('path')
+const { execFileSync } = require('child_process')
+const axios = require('axios')
 
 /**
  * Updates or downloads the youtube-dl binary.
  * @return {Promise<String>} - New version number
  */
-async function updateBinary() {
-  const bin = `youtube-dl${process.platform === "win32" ? ".exe" : ""}`;
-  const dest = join(__dirname, `ytdl`, bin);
+async function updateBinary () {
+  const bin = `youtube-dl${process.platform === 'win32' ? '.exe' : ''}`
+  const dest = join(__dirname, 'ytdl', bin)
 
   const { data } = await axios.get(
     `https://yt-dl.org/downloads/latest/${bin}`,
-    { responseType: "arraybuffer" }
-  );
+    { responseType: 'arraybuffer' }
+  )
 
-  writeFileSync(dest, data, { mode: "755", encoding: "binary" });
-  return execFileSync(dest, ["--version"]);
+  writeFileSync(dest, data, { mode: '755', encoding: 'binary' })
+  return execFileSync(dest, ['--version'])
 }
 
 if (require.main === module) {
   // CLI
-  console.log("Updating youtube-dl to the latest version...");
+  console.log('Updating youtube-dl to the latest version...')
   updateBinary()
     .then((version) => {
-      console.log(`Successfully downloaded youtube-dl ${version}`);
-      process.exit();
+      console.log(`Successfully downloaded youtube-dl ${version}`)
+      process.exit()
     })
     .catch((e) => {
-      console.error(e);
-      process.exit(1);
-    });
+      console.error(e)
+      process.exit(1)
+    })
 } else {
   // Module
-  module.exports = updateBinary;
+  module.exports = updateBinary
 }
